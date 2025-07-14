803da0dc838f4a10d9d162549974fce6e8f39daa
nelsy98 <nelsymyrna98@gmail.com>
2025-07-14 15:16:29 +0300

    Added java server for validating vendor applications

---
+++ vendor-validation-server/src/main/java/com/vendor/vendor_validation_server/VendorValidationServerApplication.java
@@ -0,0 +1,13 @@
+package com.vendor.vendor_validation_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorValidationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorValidationServerApplication.class, args);
+	}
+
+}
---
+++ vendor-validation-server/src/test/java/com/vendor/vendor_validation_server/VendorValidationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendor.vendor_validation_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorValidationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
