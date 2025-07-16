3202d18a39ce80d434ef1da3ccbe6d2d85b8f49c
lyn.ette5 <kabagaju.lynatte@students.mak.ac.ug>
2025-07-16 13:28:31 +0300

    comma

--- routes/web.php
+++ routes/web.php
@@ -77,7 +77,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
+});
     /*
     |--------------------------------------------------------------------------
     | Work Distribution
