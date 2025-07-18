d94d8eb2f95b9f61d4304aa9e46c8c7cfb7781f3
lyn.ette5 <kabagaju.lynatte@students.mak.ac.ug>
2025-07-18 14:22:42 +0300

    refactor: Update CreateOrdersTable migration for improved foreign key constraints and clean up unused code

--- database/migrations/2025_07_04_082256_create_orders_table.php
+++ database/migrations/2025_07_04_082256_create_orders_table.php
@@ -4,36 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateOrdersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('supplier_id')->nullable();
-            $table->foreign('supplier_id')->references('id')->on('suppliers')->onDelete('set null');
+            $table->foreignId('customer_id')->nullable()->constrained('customers')->onDelete('cascade');
+            $table->foreignId('supplier_id')->nullable()->constrained('suppliers')->onDelete('set null');
             $table->string('type')->nullable();
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
             $table->string('status');
             $table->timestamp('delivered_at')->nullable();
             $table->timestamp('paid_at')->nullable();
             $table->decimal('total_amount', 10, 2)->default(0);
-            $table->string('payment_status')->default('pending'); // e.g., pending, paid, failed
-            $table->string('transaction_id')->nullable(); // To store the Stripe Payment Intent ID
-            $table->foreignId('customer_id')->nullable()->constrained('customers')->onDelete('cascade');
+            $table->string('payment_status')->default('pending');
+            $table->string('transaction_id')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('orders');
     }
-};
+}
